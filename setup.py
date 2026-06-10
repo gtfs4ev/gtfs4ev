c6f87d699e26e3b1cef245715855f87b21ae5825
Jérémy Dumoulin <jeremy.dumoulin@epfl.ch>
2026-06-10 09:38:27 +0200

    Bumping to version 0.2.3 in setup.py to prepare a new pypi release to deploy latest HTML fix and new geojson export

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gtfs4ev",          
-    version="0.2.2",                  
+    version="0.2.3",                  
     description="A Python framework for simulating electric bus operations and charging demand using GTFS data",
     packages=find_packages(),
     long_description = """GTFS4EV is an open-source Python tool for planning public bus electrification using GTFS data. It provides a modular workflow that includes GTFS data pre-processing, fleet operation simulation, and scenario-based charging. Additional features include GTFS data filtering, PV integration potential, ex-post impact analysis, flexible charging strategies, spatial visualization, and dual usage as a CLI or Python library. For more information and examples, see the [GTFS4EV GitHub repository](https://github.com/gtfs4ev/gtfs4ev).""",
